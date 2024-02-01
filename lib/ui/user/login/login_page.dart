33351a936a6f31c599b3017950c91edc2f51aaab
shin-SeungHun <sng7387@gmail.com>
2024-02-01 12:39:27 +0900

    feat: create login page

---
+++ lib/ui/user/login/login_page.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:youtube_video_ai_translate_application/ui/app_bar_bottom_navigation.dart';
+import 'package:youtube_video_ai_translate_application/ui/user/register/register_page.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('로그인'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: _emailController,
+              decoration: const InputDecoration(labelText: '이메일'),
+            ),
+            const SizedBox(height: 16.0),
+            TextField(
+              controller: _passwordController,
+              decoration: const InputDecoration(labelText: '비밀번호'),
+              obscureText: true,
+            ),
+            const SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const AppBarBottomNavigation()),
+                );
+              },
+              child: const Text('로그인'),
+            ),
+            const SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const AppBarBottomNavigation()),
+                );
+              },
+              child: const Text('Google로 로그인'),
+            ),
+            const SizedBox(height: 8.0),
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const RegisterPage()),
+                );
+              },
+              child: const Text('계정이 없으신가요? 회원 가입'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
